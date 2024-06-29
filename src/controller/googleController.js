e00888f31c66dde9889356b8cbf547b1f7211aee
Pooja30031998 <85399615+Pooja30031998@users.noreply.github.com>
2024-06-29 15:46:01 +0530

    Update googleController.js

---
+++ src/controller/googleController.js
@@ -0,0 +1,33 @@
+import authRepository from "../model/repository.js";
+import UserModel from "./user-schema.js";
+import bcrypt from "bcrypt";
+
+export default class GoogleController{
+  signInSuccess = async (req, res, next) => {
+        const userData =  req.user._json;
+        const { email, name, sub } = userData;
+
+        if (email) {
+            const existingUser = await this.repository.findUser(email);
+            if (existingUser) {
+                req.session.userEmail = email;
+                return res.status(200).render("home", { userEmail: req.session.userEmail });
+            }
+            const newUser = new UserModel({
+               userName: name,
+               email: email,
+               password: sub,
+            });
+            await newUser.save();
+            req.session.userEmail = email;
+            return res.status(200).render("home", { userEmail: req.session.userEmail });
+        }else {
+            return res.status(400).render("signup", { error: "Not Authorized" });
+        }
+    }
+
+    signInFailed = (req, res, next) => {
+      return res.status(400).render("signup", { error: "Log in failure" });
+    }
+
+}
